9deba1747c3e2c97075de3a39d124939a62913d2
Mert Erdem Güneş <123267437+merterdemgunes@users.noreply.github.com>
2024-06-08 18:12:40 +0300

    Update HeroImg2.jsx

--- src/components/HeroImg2.jsx
+++ src/components/HeroImg2.jsx
@@ -1,29 +1,11 @@
 import "./HeroImg2Styles.css";
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const HeroImg2 = (props) => {
-
-  const [chosenImage, setChosenImage] = useState(window.innerWidth < 1000 ? props.imageSmall : props.image);
-
-  useEffect(() => {
-    const updateImage = () => {
-      if (window.innerWidth < 1000) {
-        setChosenImage(props.imageSmall);
-      } else {
-        setChosenImage(props.image);
-      }
-    };
-
-    window.addEventListener('resize', updateImage);
-
-    // Cleanup listener on unmount
-    return () => window.removeEventListener('resize', updateImage);
-  }, [props.image, props.imageSmall]);
-
   return (
     <div className="cont">
       <img
-        src={chosenImage} 
+        src={props.image} 
         alt="Example Image"
       />
       <div className="heading">
