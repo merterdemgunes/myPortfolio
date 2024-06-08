1c9fee4a038ff116dfc3f366fea83120bcd03162
Mert Erdem Güneş <123267437+merterdemgunes@users.noreply.github.com>
2024-06-08 18:11:44 +0300

    Update Skills.jsx

--- src/routes/Skills.jsx
+++ src/routes/Skills.jsx
@@ -4,7 +4,6 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HeroImg2 from '../components/HeroImg2';
 import imag from "../assets/blackHole8.gif"
-import imag1 from "../assets/blackhole8.jpg"
 import SkillsContent from '../components/SkillsContent';
 
 const Skills = () => {
@@ -13,7 +12,6 @@ const Skills = () => {
       <Navbar />
       <HeroImg2
         image = {imag}
-        imageSmall={imag1} 
         heading = "SKILLS"
         text = "Here is a collection of my capabilities"
       />
