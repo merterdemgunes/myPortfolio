1280d787ab81a42b26dce8334cfabaa5735d885e
Mert Erdem Güneş <123267437+merterdemgunes@users.noreply.github.com>
2024-06-08 18:10:59 +0300

    Update AboutMe.jsx

--- src/routes/AboutMe.jsx
+++ src/routes/AboutMe.jsx
@@ -5,7 +5,6 @@ import Footer from "../components/Footer";
 import HeroImg2 from '../components/HeroImg2';
 import AboutContent from '../components/AboutContent';
 import imag from "../assets/blackHole2.gif"
-import imag1 from "../assets/blackhole2.jpg"
 
 const AboutMe = () => {
   return (
@@ -13,7 +12,6 @@ const AboutMe = () => {
       <Navbar />
       <HeroImg2
         image = {imag}
-        imageSmall={imag1} 
         heading = "ABOUT"
         text = "I'm a Front-End and Back-End Developer"
       />
